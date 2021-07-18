555751d1bbe1dc11e9753ee90557311fdbbb83d7
hipolito huaylla <huaylla7@hotmail.com>
2021-07-17 21:40:21 -0400

    rails new

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+
+  QUESTIONS = {
+    1 => {category: "I am going to work"},
+    2 => {category: "?"},
+    3 => {category: ""}
+  }
+  
+  def ask
+  end
+  def answer
+  end
+  def home
+  end
+end
