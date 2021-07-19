9852940ced7341648fbbbfc6bbd5b62225fc6f8d
hipolito huaylla <huaylla7@hotmail.com>
2021-07-19 16:32:25 -0400

    02-Stupid-Coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,7 +8,17 @@ class QuestionsController < ApplicationController
   
   def ask
   end
+  
   def answer
+    @respuesta = ""
+    
+    if params["food_type"] == "I am going to work"
+      @respuesta << "Great!"
+    elsif params["food_type"].last == "?"
+      @respuesta << "Silly question, get dressed and go to work!"
+      else 
+        @respuesta << "I don't care, get dressed and go to work!"
+    end
   end
   def home
   end
